a242b39f4939b242a271098e2fa017cf7fd494b9
rishabhtatia <rishabh.tatia@codeastu.com>
2021-06-06 20:17:39 +0530

    Added JWT for authentication and validation

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,9 @@
 const router = require("express").Router();
+const auth = require("../middleware/auth");
 const User = require("../models/user");
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
@@ -34,10 +29,9 @@ router.patch("/users/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid Updates" });
   }
   try {
-    const user = await User.findByIdAndUpdate(_id, req?.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(_id);
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
     if (!user) {
       return res.status(404).send();
     }
@@ -65,11 +59,26 @@ router.delete("/users/:id", async (req, res) => {
 router.post("/users", async (req, res) => {
   try {
     const user = new User(req.body);
-    const resp = await user.save();
-    res.status(201).send(user);
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (err) {
     console.log(err);
     res.status(400).send(err);
+  }
+});
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req?.body?.email,
+      req?.body?.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
   }
 });
 
