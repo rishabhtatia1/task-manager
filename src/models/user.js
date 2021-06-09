f567dde2efa5174c3707f3b04622ff4552138ffe
rishabhtatia <rishabh.tatia@codeastu.com>
2021-06-09 22:55:20 +0530

    Added pagination, sorting and filtering for tasks

--- src/models/user.js
+++ src/models/user.js
@@ -4,40 +4,43 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true,
-    trim: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  age: { type: Number, default: 0 },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password cannot contain password");
-      }
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
       },
     },
-  ],
-});
+    age: { type: Number, default: 0 },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password cannot contain password");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 userSchema.virtual("tasks", {
   ref: "Task",
--- src/routers/task.js
+++ src/routers/task.js
@@ -2,9 +2,31 @@ const router = require("express").Router();
 const auth = require("../middleware/auth");
 const Task = require("../models/task");
 
+//GET /tasks?completed=true
+//GET /tasks?limit=2&skip=0
+//GET /tasks?sortBy=createdAt_desc
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+  if (req?.query?.completed) {
+    match.completed = req.query.completed === "true";
+  }
+  if (req?.query?.sortBy) {
+    const parts = req.query.sortBy.split("_");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
   try {
-    await req.user.populate("tasks").execPopulate();
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req?.query?.limit),
+          skip: parseInt(req?.query?.skip),
+          sort,
+        },
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (err) {
     console.log(err);
